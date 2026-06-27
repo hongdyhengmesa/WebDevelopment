dde37de664e48a103cab7216b2027083df860aef
Hong Dihengmesa <dyhengmesa@Dyhengmesas-MacBook-Pro.local>
2026-06-27 14:27:41 +0700

    Update about App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="container">
     <Databiding />
+    <FirstDatabinding/>
   </div>
 </template>
 
 <script setup>
 import  Databiding  from "./components/Databinding.vue";
+import FirstDatabinding from "./components/FirstDatabinding.vue";
 </script>
