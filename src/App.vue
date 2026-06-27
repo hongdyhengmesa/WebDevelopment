3a74689117005e020cd483d3a409ca95007fd3b5
Hong Dihengmesa <dyhengmesa@Dyhengmesas-MacBook-Pro.local>
2026-06-28 00:15:26 +0700

    add Practice1 and add more file App

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <div class="container">
     <Databiding />
     <FirstDatabinding/>
+    <Practice1 />
   </div>
 </template>
 
 <script setup>
-import  Databiding  from "./components/Databinding.vue";
-import FirstDatabinding from "./components/FirstDatabinding.vue";
+import  Databiding  from "./components/databinding/Databinding.vue";
+import FirstDatabinding from "./components/databinding/FirstDatabinding.vue";
+import Practice1 from "./components/databinding/Practice1.vue";
 </script>
---
+++ src/components/databinding/Practice1.vue
@@ -0,0 +1,9 @@
+<template>
+    <h3>Your first name is: {{ firstName }}</h3>
+    <h3>Your last name is: {{ lastName }}</h3>
+</template>
+
+<script setup>
+   const firstName = "Dihengmesa";
+   const lastName = "HONG"
+</script>
