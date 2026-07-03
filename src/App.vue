97b1c6d2684edb464f99a3c2607b1c961082a93b
Hong Dihengmesa <dyhengmesa@Dyhengmesas-MacBook-Pro.local>
2026-07-03 12:05:21 +0700

    loop redering (v-for)

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <template>
   <div class="container">
-    <Vif/>
-    <VElse/>
-    <V_else_if/>
-    <Practice1/>
-    <Practice2/>
-    <Practice3/>
-    <Practice4/>
+    <V_for/>
   </div>
 </template>
 
 <script setup>
-  import Vif from "./components/conditions/Vif.vue";
-  import VElse from "./components/conditions/VElse.vue";
-  import V_else_if from "./components/conditions/V_else_if.vue";
-  import Practice1 from "./components/conditions/Practice1.vue";
-  import Practice2 from "./components/conditions/Practice2.vue";
-  import Practice3 from "./components/conditions/Practice3.vue";
-  import Practice4 from "./components/conditions/Practice4.vue";
+  import V_for from "./components/loop/V_for.vue";
 </script>
---
+++ src/components/loop/V_for.vue
@@ -0,0 +1,39 @@
+<template>
+    <p v-for="fruit in fruits" :key="fruit">{{ fruit }}</p>
+    <h6 v-for="number in numbers" :key="number">{{ number }}</h6>
+    <h3 v-for="student in students" :key="student">{{ student }}</h3>
+    <h5 v-for="langauge in langauges" :key="langauge">{{ langauge }}</h5>
+</template>
+
+<script setup>
+    const fruits = [
+        "Banana",
+        "Mango",
+        "Potato"
+    ]
+
+    const numbers = [
+        10,
+        20,
+        30,
+        40,
+        50,
+    ]
+
+    const students = [
+        "John",
+        "Kimmy",
+        "Lalo",
+        "Phenh",
+        "Noly"
+    ]
+
+    const langauges = [
+        "Html",
+        "Javascript",
+        "Css",
+        "VueJs",
+        "NestJs"
+    ]
+
+</script>
