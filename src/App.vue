3bb93e8569602532402fede0e81f3c5b11008633
Hong Dihengmesa <dyhengmesa@Dyhengmesas-MacBook-Pro.local>
2026-07-03 22:45:17 +0700

    finished practice2

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     <Ex1/>
     <p>--------------------- [Practice]---------------------</p>
     <Practice1/>
+    <Practice2/>
   </div>
 </template>
 
@@ -12,4 +13,5 @@
   import V_for from "./components/loop/V_for.vue";
   import Ex1 from "./components/loop/Ex1.vue";
   import Practice1 from "./components/loop/Practice1.vue";
+  import Practice2 from "./components/loop/Practice2.vue";
 </script>
---
+++ src/components/loop/Practice2.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-for="employee in employees" :key="employee.id">
+        <p>{{ employee.name }}</p>
+        <p>{{ employee.position }}</p>
+    </div>
+</template>
+
+<script setup>
+    const employees = [
+        {
+            id: 1,
+            name: "Neymar",
+            position: "Developer"
+        },
+        {
+            id: 2,
+            name: "Ronaldo",
+            position: "Designer"
+        },
+        {
+            id: 3,
+            name: "Messi",
+            position: "Full stack"
+        }
+    ]
+</script>
