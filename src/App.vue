7801020ed7160adcea46c67e89c0f4d5f763c904
Hong Dihengmesa <dyhengmesa@Dyhengmesas-MacBook-Pro.local>
2026-07-04 13:06:08 +0700

    v-show finished

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
   <div class="container">
-    <V_for/>
-    <p>---------------------</p>
     <Ex1/>
-    <p>--------------------- [Practice]---------------------</p>
-    <Practice1/>
-    <Practice2/>
   </div>
 </template>
 
 <script setup>
-  import V_for from "./components/loop/V_for.vue";
-  import Ex1 from "./components/loop/Ex1.vue";
-  import Practice1 from "./components/loop/Practice1.vue";
-  import Practice2 from "./components/loop/Practice2.vue";
+  import Ex1 from "./components/v-show/Ex1.vue";
 </script>
---
+++ src/components/v-show/Ex1.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1 v-show="isVisible">Hello Vue</h1>
+    <h2 v-show="online">Online</h2>
+</template>
+<script setup>
+    const isVisible = true
+    const online = true
+</script>
