ee99904b85bfb629adc7206421bd1feb76e6abc8
Hong Dihengmesa <dyhengmesa@Dyhengmesas-MacBook-Pro.local>
2026-07-10 01:05:06 +0700

    axios

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <template>
-    <div class="container">
-        <UserParent/>
-        <CounterParent/>
-    </div>
+    
 </template>
 
 <script setup>
-    import UserParent from "./components/emits/UserParent.vue";
-    import CounterParent from "./components/emits/CounterParent.vue";
+    
+
 </script>
---
+++ src/components/axios/User.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Users</h1>
+    <div v-for="user in users" :key="user">
+        {{ user.id }}
+        {{ user.name }}
+    </div>
+</template>
+
+<script setup>
+    import { ref, onMounted } from 'vue';
+    import axios from 'axios';
+
+    const users = ref([])
+
+    async function getUsers(){
+        const response = await axios.get(
+            "https://jsonplaceholder.typicode.com/users"
+        )
+        users.value = response.data
+    }
+
+    onMounted(() =>{
+        getUsers()
+    })
+
+</script>
