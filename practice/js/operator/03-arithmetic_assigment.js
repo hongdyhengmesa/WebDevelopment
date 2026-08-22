932fd6626a4ecf19624f8177170cc19a7de2caad
Hong Dihengmesa <dyhengmesa@Dyhengmesas-MacBook-Pro.local>
2026-08-23 00:46:58 +0700

    add rock game

--- practice/js/operator/03-arithmetic_assigment.js
+++ practice/js/operator/03-arithmetic_assigment.js
@@ -1,11 +1,27 @@
+// 1.Calculate for Sum
 let totalPrice = 100;
 let tax = 10;
 
 totalPrice += tax; // totalPrice = totalPrice + tax (100 + 10 = 110)
 console.log(totalPrice);
 
+// 2.Calculate for Minus
 let totalPrice1 = 300;
 let tax1 = 100;
 
 totalPrice1 -= tax1;
 console.log(totalPrice1)
+
+// 3.Calculate  for Multiple
+let totalMulti = 100;
+let taxMulti = 10;
+
+totalMulti *= taxMulti;
+console.log(totalMulti)
+
+// 4.Calculate for Division
+let totalDivi = 20;
+let taxDivi = 10;
+
+totalDivi /= taxDivi;
+console.log(totalDivi)
